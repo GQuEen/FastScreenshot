d1d4b32453ab1dc603f518052263457136af3d6e
SugarGirl <chengege1004@gmail.com>
2017-03-08 17:59:48 +0800

    widgetUI 初步完成

--- FastScreenshotWidget/TodayViewController.m
+++ FastScreenshotWidget/TodayViewController.m
@@ -12,6 +12,10 @@
 
 @interface TodayViewController () <NCWidgetProviding>
 
+@property (strong, nonatomic) UIImageView *screenshotImageView;//显示截图的imageView
+@property (strong, nonatomic) UIScrollView *shareScorllView;//装分享按钮的scroll
+@property (strong, nonatomic) UIButton *moreButton;//向右点击滑动的按钮
+
 @end
 
 @implementation TodayViewController
@@ -26,23 +30,22 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    self.preferredContentSize = CGSizeMake(0, 110);
+    self.preferredContentSize = CGSizeMake(375, 110);
     
-    UIImageView *imageView1 = [[UIImageView alloc]initWithFrame:CGRectMake(15, 10, 75, 90)];
-    imageView1.backgroundColor = [UIColor greenColor];
-    imageView1.contentMode = UIViewContentModeScaleAspectFill;
-    imageView1.layer.cornerRadius = 5;
-    imageView1.layer.masksToBounds = YES;
+    _screenshotImageView = [[UIImageView alloc]initWithFrame:CGRectMake(15, 10, 75, 90)];
+    _screenshotImageView.backgroundColor = [UIColor greenColor];
+    _screenshotImageView.contentMode = UIViewContentModeScaleAspectFill;
+    _screenshotImageView.layer.cornerRadius = 5;
+    _screenshotImageView.layer.masksToBounds = YES;
     
-    NSLog(@"2121");
-    
-    [self.view addSubview:imageView1];
+    [self.view addSubview:_screenshotImageView];
     
     //按时间顺序排序并获取资源
     PHFetchOptions *options = [[PHFetchOptions alloc] init];
     options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:YES]];
     PHFetchResult *assetsFetchResults = [PHAsset fetchAssetsWithOptions:options];
     
+    __weak typeof(self) weakSelf = self;
     //请求图片
     PHCachingImageManager *imageManager = [[PHCachingImageManager alloc] init];
     PHAsset *asset = assetsFetchResults[assetsFetchResults.count-2];
@@ -53,9 +56,47 @@
                          resultHandler:^(UIImage *result, NSDictionary *info) {
                              
                              // 得到一张 UIImage，展示到界面上
-                             imageView1.image = result;
+                             weakSelf.screenshotImageView.image = result;
                              
                          }];
+    [self setupScroll];
+    [self setupMoreButton];
+    [self setupShareButton];
+}
+
+- (void)setupScroll {
+    _shareScorllView = [[UIScrollView alloc]initWithFrame:CGRectMake(105, 30, 375-75-30-41-15, 51)];
+    _shareScorllView.showsVerticalScrollIndicator = NO;
+    _shareScorllView.showsHorizontalScrollIndicator = NO;
+    _shareScorllView.contentSize = CGSizeMake(self.shareScorllView.frame.size.width*2, self.shareScorllView.frame.size.height);
+    _shareScorllView.pagingEnabled = YES;
+    //_shareScorllView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.shareScorllView];
+}
+
+- (void)setupMoreButton {
+    _moreButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    _moreButton.frame = CGRectMake(CGRectGetMaxX(self.shareScorllView.frame), 0, 26, 26);
+    _moreButton.center = CGPointMake(CGRectGetMidX(self.moreButton.frame), self.view.frame.size.height/2);
+    [_moreButton setImage:[UIImage imageNamed:@"wg_next"] forState:UIControlStateNormal];
+    [self.view addSubview:self.moreButton];
+}
+
+- (void)setupShareButton {
+    
+    NSArray *imageDateArray = @[@"share_weibo",@"share_wechat",@"share_qq",@"share_moments"];
+    for (int i = 0; i < imageDateArray.count; i ++) {
+        UIButton *shareBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        shareBtn.tag = i;
+        shareBtn.frame = CGRectMake(i*71, 0, 51, 51);
+        [shareBtn setImage:[UIImage imageNamed:imageDateArray[i]] forState:UIControlStateNormal];
+        [_shareScorllView addSubview:shareBtn];
+        [shareBtn addTarget:self action:@selector(clickShareBtn:) forControlEvents:UIControlEventTouchUpInside];
+    }
+}
+
+- (void)clickShareBtn:(UIButton *)sender {
+    
 
 }
 
